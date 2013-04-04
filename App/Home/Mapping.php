9b2d28a9b427a58cf320f57435553e67b6cbdab5
MinoWu <minowu@foxmail.com>
2013-04-05 01:52:06 +0800

    添加Mapping文件。

---
+++ App/Home/Mapping.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Home;
+/**
+ * Project app config
+ * Copyright (c) 2004-2013 Methink
+ * minowu@foxmail.com
+ */
+
+use meSmart;
+
+/**
+ * App group config
+ */
+class Mapping extends meSmart\Mapping {
+}
