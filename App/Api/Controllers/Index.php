fbbe26758274e86c4e2b2db7b6f45c7da1531537
MinoWu <minowu@foxmail.com>
2013-04-11 17:59:04 +0800

    添加API功能。

---
+++ App/Api/Controllers/Index.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Api\Controllers;
+
+use meSmart\Controllers\Base as Controller;
+use meSmart\Models\Base as Model;
+
+class Index extends Controller {
+}
---
+++ App/Api/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Api\Models;
+
+use meSmart\Models\Base as Model;
+
+class User extends Model {
+
+	public function createnew()
+	{
+		dump('create new');
+	}
+}
